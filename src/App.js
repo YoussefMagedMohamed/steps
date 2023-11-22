7c8d6d0f934e84cc4ae9a33196c82fd9810e4e8b
YoussefMagedMohamed <joeyoussef307@gmail.com>
2023-11-22 21:52:41 +0200

    Static Data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from 'react'
-import "./index.css"
+import React from "react";
+import "./index.css";
 
 const App = () => {
-  return (
-    <div className='steps'>Hello</div>
-  )
-}
+  const messages = [
+    "Learn React ⚛️",
+    "Apply for jobs 💼",
+    "Invest your new income 🤑",
+  ];
 
-export default App
+  const step = 1;
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <div className="message">
+        <p>
+          Step{step}: {messages[step - 1]}
+        </p>
+      </div>
+
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
